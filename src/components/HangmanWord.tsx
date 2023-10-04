d1a2739a7c4ad8b57c44be4d2a8e37ea6721d447
Denis <haosmark@gmail.com>
2023-10-04 10:43:52 -0500

    refactor for readability

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -7,18 +7,30 @@ type HangmanDrawingProps = {
 }
 
 export const HangmanWord = ({ reveal = false, guessedLetters, wordToGuess }: HangmanDrawingProps) => {
+  const renderWord = () => {
+    return wordToGuess.split("").map((letter, i) => renderLetter(letter, i))
+  }
+
+  const renderLetter = (letter: string, i: number) => {
+    const isGuessed = guessedLetters.includes(letter);
+    const shouldReveal = isGuessed || reveal;
+    const textColor = isGuessed && reveal ? "red" : "black"
+
+    return (
+      <span className={styles.letter} key={i}>
+        <span style={{
+          visibility: shouldReveal ? "visible" : "hidden",
+          color: textColor
+        }}>
+          {letter}
+        </span>
+      </span>
+    )
+  }
+
   return (
     <div className={styles.wordContainer}>
-      {wordToGuess.split("").map((letter, i) => (
-        <span className={styles.letter} key={i}>
-          <span style={{ 
-            visibility: guessedLetters.includes(letter) || reveal ? "visible" : "hidden",
-            color: !guessedLetters.includes(letter) && reveal ? "red" : "black" 
-          }}>
-            {letter}
-          </span>
-        </span>
-      ))}
+      {renderWord()}
     </div>
   )
 }
